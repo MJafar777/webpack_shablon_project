9bc863ceb838ddd171c15bd12b62c21bf23828b2
MJafar777 <94430558+MJafar777@users.noreply.github.com>
2023-12-11 12:15:29 +0500

    Update MainPage.tsx

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-    const { t } = useTranslation();
-    const [value, setValue] = useState('');
+  const { t } = useTranslation();
 
-    const onChange = (val: string) => {
-        setValue(val);
-    };
-
-    return (
-        <div data-testid="MainPage">
-            <div>123123123123123123</div>
-            {t('Главная страница')}
-        </div>
-    );
+  return <div data-testid="MainPage" />;
 };
 
 export default MainPage;
